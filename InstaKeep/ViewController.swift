4b00de479312d951298e63efe7400176ea0e8f09
wendy <w2699033@gmail.com>
2025-07-25 14:45:35 +0800

    Re-initialize Git in correct folder

---
+++ InstaKeep/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstaKeep
+//
+//  Created by Wendy Hsiao on 2025/7/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("test")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ InstaKeepTests/InstaKeepTests.swift
@@ -0,0 +1,17 @@
+//
+//  InstaKeepTests.swift
+//  InstaKeepTests
+//
+//  Created by Wendy Hsiao on 2025/7/25.
+//
+
+import Testing
+@testable import InstaKeep
+
+struct InstaKeepTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
